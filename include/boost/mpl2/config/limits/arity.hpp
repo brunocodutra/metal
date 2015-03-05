e6cf47550e06a37c69ee08a9038f5513ae514a1e
Bruno Dutra <brunocodutra@gmail.com>
2015-03-05 01:10:49 -0300

    setting BOOST_MPL_LIMIT_METAFUNCTION_ARITY to SIZE_MAX
    SIZE_MAX is defined as the greatest value returned by sizeof...()
    in case SIZE_MAX is not defined, UINT_MAX is used instead

--- include/boost/mpl2/config/limits/arity.hpp
+++ include/boost/mpl2/config/limits/arity.hpp
@@ -8,7 +8,17 @@
 #define _BOOST_MPL_CONFIG_LIMITS_ARITY_HPP_
 
 #if !defined(BOOST_MPL_LIMIT_METAFUNCTION_ARITY)
-    #define BOOST_MPL_LIMIT_METAFUNCTION_ARITY 20
+    #include <boost/mpl2/config/switches/variadic.hpp>
+
+    #if defined(BOOST_MPL_CFG_NO_VARIADIC_TEMPLATES)
+        #define BOOST_MPL_LIMIT_METAFUNCTION_ARITY 20U
+    #else
+        #include <cstdint>
+        #if !defined(SIZE_MAX)
+            #define SIZE_MAX UINT_MAX
+        #endif
+        #define BOOST_MPL_LIMIT_METAFUNCTION_ARITY SIZE_MAX
+    #endif
 #endif
 
 #endif
