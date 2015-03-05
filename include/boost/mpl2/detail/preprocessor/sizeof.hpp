d83769548d83aded9c18f7a1e0485c2d4a81a541
Bruno Dutra <brunocodutra@gmail.com>
2015-03-05 01:11:56 -0300

    avoiding unecessary inclusion of headers

--- include/boost/mpl2/detail/preprocessor/sizeof.hpp
+++ include/boost/mpl2/detail/preprocessor/sizeof.hpp
@@ -7,17 +7,17 @@
 #ifndef _BOOST_MPL_DETAIL_PREPROCESSOR_SIZEOF_HPP_
 #define _BOOST_MPL_DETAIL_PREPROCESSOR_SIZEOF_HPP_
 
-#include <boost/mpl2/config/limits/arity.hpp>
 #include <boost/mpl2/config/switches/variadic.hpp>
-
-#include <boost/mpl2/detail/preprocessor/args.hpp>
-
-#include <boost/preprocessor/repeat.hpp>
 
 #if !defined(BOOST_MPL_CFG_NO_VARIADIC_TEMPLATES)
     #define BOOST_MPL_SEIZEOF(_, ARGS) \
         sizeof...(ARGS)
 #else
+    #include <boost/mpl2/config/limits/arity.hpp>
+    #include <boost/mpl2/detail/preprocessor/args.hpp>
+
+    #include <boost/preprocessor/repeat.hpp>
+
     #define BOOST_MPL_DETAIL_SPECIALIZE_SEIZEOF(Z, N, _) \
         template<BOOST_MPL_PARAMS(N, items)> \
         struct sizeof_<BOOST_MPL_ARGS(N, items)> \
