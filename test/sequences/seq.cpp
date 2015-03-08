f226a91b4f88774857899f812f0274bfc4ff07fc
Bruno Dutra <brunocodutra@gmail.com>
2015-03-08 19:40:33 -0300

    organizing the test directory so that it mirrors the include hierarchy

--- test/sequences/seq.cpp
+++ test/sequences/seq.cpp
@@ -6,19 +6,12 @@
 
 #define BOOST_MPL2_LIMIT_METAFUNCTION_ARITY 5
 
-#include <boost/mpl2/detail/preprocessor/typeof.hpp>
-#include <boost/mpl2/detail/preprocessor/sizeof.hpp>
 #include <boost/mpl2/sequences/seq.hpp>
 
 typedef boost::mpl2::seq<>::type empty;
 typedef boost::mpl2::seq<int, void, float>::type seq;
 
-//template<typename>
-//struct test;
-
-//test<seq> a;
-
-BOOST_MPL2_TYPEOF(0+0) main()
+int main()
 {
     return seq::size + empty::size != 3;
 }
