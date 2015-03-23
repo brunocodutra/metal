fa2df995cd6abef9937853b31ee552b59ec4d79f
Bruno Dutra <brunocodutra@gmail.com>
2015-03-23 19:19:00 -0300

    splitting helper to strip parenthesis from types off from assert
    removing  redundant assertion calls in order to keep the interface
    minimal

--- include/boost/mpl2/core/assert.hpp
+++ include/boost/mpl2/core/assert.hpp
@@ -5,38 +5,14 @@
 #ifndef BOOST_MPL2_CORE_ASSERT_HPP
 #define BOOST_MPL2_CORE_ASSERT_HPP
 
-#include <boost/mpl2/core/logical/not.hpp>
+#include <boost/mpl2/core/detail/type.hpp>
 
 #include <boost/static_assert.hpp>
 
-namespace boost
-{
-    namespace mpl2
-    {
-        namespace detail
-        {
-            template<typename>
-            struct assert_;
-
-            template<typename T>
-            struct assert_<void (T)> : T {};
-        }
-    }
-}
-
 #define BOOST_MPL2_ASSERT(PRED) \
-    BOOST_STATIC_ASSERT(boost::mpl2::detail::assert_<void (PRED)>::type::value)
-
-#define BOOST_MPL2_ASSERT_NOT(PRED) \
-    BOOST_STATIC_ASSERT(boost::mpl2::not_<boost::mpl2::detail::assert_<void (PRED)> >::type::value)
+    BOOST_STATIC_ASSERT(BOOST_MPL2_DETAIL_TYPE(PRED)::type::value)
 
 #define BOOST_MPL2_ASSERT_MSG(PRED, MSG) \
-    BOOST_STATIC_ASSERT_MSG(boost::mpl2::detail::assert_<void (PRED)>::type::value, MSG)
-
-#define BOOST_MPL2_ASSERT_MSG_NOT(PRED, MSG) \
-    BOOST_STATIC_ASSERT_MSG(boost::mpl2::not_<boost::mpl2::detail::assert_<void (PRED)> >::type::value, MSG)
-
-#define BOOST_MPL2_ASSERT_RELATION(X, OP, Y) \
-    BOOST_STATIC_ASSERT(X OP Y)
+    BOOST_STATIC_ASSERT_MSG(BOOST_MPL2_DETAIL_TYPE(PRED)::type::value, MSG)
 
 #endif
---
+++ include/boost/mpl2/core/detail/type.hpp
@@ -0,0 +1,30 @@
+// Copyright Bruno Dutra 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_MPL2_CORE_DETAIL_TYPE_HPP
+#define BOOST_MPL2_CORE_DETAIL_TYPE_HPP
+
+#include <boost/mpl2/core/identity.hpp>
+
+namespace boost
+{
+    namespace mpl2
+    {
+        namespace detail
+        {
+            template<typename>
+            struct strip_parenthesis;
+
+            template<typename T>
+            struct strip_parenthesis<void (T)> :
+                identity<T>
+            {};
+        }
+    }
+}
+
+#define BOOST_MPL2_DETAIL_TYPE(T) \
+    boost::mpl2::detail::strip_parenthesis<void (T)>
+
+#endif
