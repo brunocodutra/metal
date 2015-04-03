f77c8df76438eaebf8d548a0a45d5489540ce953
Bruno Dutra <brunocodutra@gmail.com>
2015-04-03 11:53:02 -0300

    updating type_trait test

--- test/core/nested_type_trait.cpp
+++ test/core/nested_type_trait.cpp
@@ -2,55 +2,126 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
+#include <boost/mpl2/integrals/boolean.hpp>
+#include <boost/mpl2/integrals/logical/not.hpp>
 #include <boost/mpl2/core/nested_type_trait.hpp>
-
-struct test01;
-struct test02 {};
-struct test03 {void nested();};
-struct test04 {int nested;};
-struct test05 {static void nested();};
-struct test06 {static int nested;};
-struct test07 {typedef int nested;};
-struct test08 {struct nested;};
-struct test09 {typedef struct {} nested;};
-struct test10 {typedef void (nested)();};
-struct test11 {typedef void (*nested)();};
-struct test12 {typedef void (&nested)();};
-struct test13 {typedef char nested[1];};
-struct test14 {typedef char (&nested)[1];};
-struct test15 {enum nested {};};
-struct test16 {enum class nested {};};
-struct test17 {template<typename> void nested();};
-struct test18 {template<typename> struct nested;};
-struct test19 {template<typename...> struct nested;};
-struct test20 {template<bool> struct nested;};
-struct test21 {template<typename type, type> struct nested;};
-struct test22 {template<template<typename> class> struct nested;};
+#include <boost/mpl2/core/assert.hpp>
 
 BOOST_MPL2_DEFINE_NESTED_TYPE_TRAIT(has_nested, nested);
 
+#define BOOST_MPL2_DETAIL_HAS_NESTED(X) \
+    BOOST_MPL2_ASSERT(has_nested<X>); \
+    BOOST_MPL2_ASSERT(has_nested<X>::type); \
+    BOOST_MPL2_ASSERT(has_nested_t<X>); \
+    static_assert(has_nested<X>{}, "has_nested<X>{}"); \
+    static_assert(has_nested<X>{}(), "has_nested<X>{}()") \
+/**/
+
+#define BOOST_MPL2_DETAIL_NO_NESTED(X) \
+    BOOST_MPL2_ASSERT(boost::mpl2::not_<has_nested<X> >); \
+    BOOST_MPL2_ASSERT(boost::mpl2::not_<has_nested<X>::type>); \
+    BOOST_MPL2_ASSERT(boost::mpl2::not_<has_nested_t<X> >); \
+    static_assert(!has_nested<X>{}, "!has_nested<X>{}"); \
+    static_assert(!has_nested<X>{}(), "!has_nested<X>{}()") \
+/**/
+
+BOOST_MPL2_DETAIL_NO_NESTED(int);
+BOOST_MPL2_DETAIL_NO_NESTED(int[]);
+BOOST_MPL2_DETAIL_NO_NESTED(int const volatile[]);
+BOOST_MPL2_DETAIL_NO_NESTED(int(&)[]);
+BOOST_MPL2_DETAIL_NO_NESTED(int const volatile(&)[]);
+BOOST_MPL2_DETAIL_NO_NESTED(int*);
+BOOST_MPL2_DETAIL_NO_NESTED(int const volatile* const volatile);
+BOOST_MPL2_DETAIL_NO_NESTED(int&);
+BOOST_MPL2_DETAIL_NO_NESTED(int const volatile&);
+BOOST_MPL2_DETAIL_NO_NESTED(int&&);
+BOOST_MPL2_DETAIL_NO_NESTED(int const volatile&&);
+
+enum test_enum{nested};
+BOOST_MPL2_DETAIL_NO_NESTED(test_enum);
+
+enum class test_enum_class{nested};
+BOOST_MPL2_DETAIL_NO_NESTED(test_enum_class);
+
+using test_function_alias = void();
+BOOST_MPL2_DETAIL_NO_NESTED(test_function_alias);
+
+using test_function_pointer_alias = void(*)();
+BOOST_MPL2_DETAIL_NO_NESTED(test_function_pointer_alias);
+
+using test_function_reference_alias = void(&)();
+BOOST_MPL2_DETAIL_NO_NESTED(test_function_reference_alias);
+
+struct test_undefined_struct;
+BOOST_MPL2_DETAIL_NO_NESTED(test_undefined_struct);
+
+struct test_empty_struct {};
+BOOST_MPL2_DETAIL_NO_NESTED(test_empty_struct);
+
+struct test_member {int nested;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_member);
+
+struct test_static_member {static int nested;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_static_member);
+
+struct test_member_function {void nested();};
+BOOST_MPL2_DETAIL_NO_NESTED(test_member_function);
+
+struct test_static_member_function {static void nested();};
+BOOST_MPL2_DETAIL_NO_NESTED(test_static_member_function);
+
+struct test_nested_alias {using nested = void;};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_alias);
+
+struct test_nested_template_function_alias {template<typename> using nested = int;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_template_function_alias);
+
+struct test_nested_struct {struct nested;};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_struct);
+
+struct test_nested_function_alias {using nested = void();};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_function_alias);
+
+struct test_nested_function_pointer_alias {using nested = void(*)();};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_function_pointer_alias);
+
+struct test_nested_function_reference_alias {using nested = void(&)();};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_function_reference_alias);
+
+struct test_nested_array_alias {typedef char nested[1];};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_array_alias);
+
+struct test_nested_array_reference_alias {typedef char (&nested)[1];};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_array_reference_alias);
+
+struct test_nested_enum {enum nested {};};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_enum);
+
+struct test_nested_enum_class {enum class nested {};};
+BOOST_MPL2_DETAIL_HAS_NESTED(test_nested_enum_class);
+
+struct test_nested_enum_value {enum {nested};};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_enum_value);
+
+struct test_nested_template_function {template<typename> void nested();};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_template_function);
+
+struct test_nested_template {template<typename> struct nested;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_template);
+
+struct test_nested_variadic_template {template<typename...> struct nested;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_variadic_template);
+
+struct test_nested_non_type_template {template<bool> struct nested;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_non_type_template);
+
+struct test_nested_mixed_template {template<typename type, type> struct nested;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_mixed_template);
+
+struct test_nested_template_template {template<template<typename> class> struct nested;};
+BOOST_MPL2_DETAIL_NO_NESTED(test_nested_template_template);
+
 int main()
 {
-    return  has_nested<test01>() ||
-            has_nested<test02>() ||
-            has_nested<test03>() ||
-            has_nested<test04>() ||
-            has_nested<test05>() ||
-            has_nested<test06>() ||
-           !has_nested<test07>() ||
-           !has_nested<test08>() ||
-           !has_nested<test09>() ||
-           !has_nested<test10>() ||
-           !has_nested<test11>() ||
-           !has_nested<test12>() ||
-           !has_nested<test13>() ||
-           !has_nested<test14>() ||
-           !has_nested<test15>() ||
-           !has_nested<test16>() ||
-            has_nested<test17>() ||
-            has_nested<test18>() ||
-            has_nested<test19>() ||
-            has_nested<test20>() ||
-            has_nested<test21>() ||
-            has_nested<test22>();
+    return 0;
 }
