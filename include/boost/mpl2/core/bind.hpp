0f935c2011956d866a89f320cf3193457ec15260
Bruno Dutra <brunocodutra@gmail.com>
2015-04-04 00:49:31 -0300

    introducing bind and accompanying protect

---
+++ include/boost/mpl2/core/bind.hpp
@@ -0,0 +1,59 @@
+// Copyright Bruno Dutra 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_MPL2_CORE_BIND_HPP
+#define BOOST_MPL2_CORE_BIND_HPP
+
+#include <boost/mpl2/core/arg.hpp>
+#include <boost/mpl2/core/protect.hpp>
+#include <boost/mpl2/core/invariant.hpp>
+#include <boost/mpl2/core/apply.hpp>
+
+namespace boost
+{
+    namespace mpl2
+    {
+        template<typename nullfctnl, typename... params>
+        struct bind
+        {
+        private:
+            template<typename param>
+            struct parse
+            {
+                template<typename... args>
+                using type = apply<invariant<param>, args...>;
+            };
+
+            template<typename param>
+            struct parse<protect<param> >
+            {
+                template<typename... args>
+                using type = apply<invariant<protect<param> >, args...>;
+            };
+
+            template<std::size_t n>
+            struct parse<arg<n> >
+            {
+                template<typename... args>
+                using type = apply<arg<n>, args...>;
+            };
+
+            template<typename nf, typename... prms>
+            struct parse<bind<nf, prms...> >
+            {
+                template<typename... args>
+                using type = apply<bind<nf, prms...>, args...>;
+            };
+
+        public:
+            template<typename... args>
+            using type = apply<
+                typename apply<parse<nullfctnl>, args...>::type,
+                typename apply<parse<params>, args...>::type...
+            >;
+        };
+    }
+}
+
+#endif
