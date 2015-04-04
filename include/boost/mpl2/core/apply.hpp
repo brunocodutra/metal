5cfef85d0aef840f3cea3c095416df66b9a481c9
Bruno Dutra <brunocodutra@gmail.com>
2015-04-03 21:08:10 -0300

    introducing apply
    forwards args to the function returned by a nullary functional

---
+++ include/boost/mpl2/core/apply.hpp
@@ -0,0 +1,17 @@
+// Copyright Bruno Dutra 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_MPL2_CORE_APPLY_HPP
+#define BOOST_MPL2_CORE_APPLY_HPP
+
+namespace boost
+{
+    namespace mpl2
+    {
+        template<typename f, typename... args>
+        using apply = typename f::template type<args...>;
+    }
+}
+
+#endif
