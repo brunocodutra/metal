00ce8a070a9df302d4245d569b913fc3db1e1480
Bruno Dutra <brunocodutra@gmail.com>
2015-04-05 20:06:48 -0300

    using namespace rather than single names on tests

--- test/integrals/arithmetic.cpp
+++ test/integrals/arithmetic.cpp
@@ -2,18 +2,15 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
-#include <boost/mpl2/core/assert.hpp>
 #include <boost/mpl2/integrals/arithmetic.hpp>
 #include <boost/mpl2/integrals/integer.hpp>
 #include <boost/mpl2/integrals/comparison/equal_to.hpp>
+#include <boost/mpl2/core/assert.hpp>
 
-using boost::mpl2::inc;
-using boost::mpl2::dec;
-using boost::mpl2::negate;
-using boost::mpl2::equal_to;
+using namespace boost::mpl2;
 
-typedef boost::mpl2::int_<0> zero;
-typedef boost::mpl2::int_<1> one;
+typedef int_<0> zero;
+typedef int_<1> one;
 
 BOOST_MPL2_ASSERT((equal_to<inc<zero>, one>));
 BOOST_MPL2_ASSERT((equal_to<dec<zero>, negate<one> >));
