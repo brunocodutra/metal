c4931bb95c439df4b5a355005fc97c46aad07c5d
Bruno Dutra <brunocodutra@gmail.com>
2015-04-05 21:30:48 -0300

    improving tests for logical operators

--- test/integrals/logical.cpp
+++ test/integrals/logical.cpp
@@ -10,15 +10,60 @@
 using namespace boost::mpl2;
 
 struct undefined;
-
 typedef long_<0> zero;
 typedef ushort_<1> one;
 
-BOOST_MPL2_ASSERT((or_<zero, zero, one, undefined, zero>));
-BOOST_MPL2_ASSERT((not_<or_<zero, zero, zero, zero, zero> >));
+BOOST_MPL2_ASSERT((or_<zero, one>));
+BOOST_MPL2_ASSERT((or_<zero, zero, one>));
+BOOST_MPL2_ASSERT((or_<zero, zero, zero, one>));
+BOOST_MPL2_ASSERT((or_<zero, zero, zero, zero, one>));
+BOOST_MPL2_ASSERT((or_<zero, zero, zero, zero, zero, one>));
+BOOST_MPL2_ASSERT((or_<zero, zero, zero, zero, zero, zero, one>));
+BOOST_MPL2_ASSERT((or_<zero, zero, zero, zero, zero, zero, zero, one>));
 
+BOOST_MPL2_ASSERT((not_<or_<zero, zero> >));
+BOOST_MPL2_ASSERT((not_<or_<zero, zero, zero> >));
+BOOST_MPL2_ASSERT((not_<or_<zero, zero, zero, zero> >));
+BOOST_MPL2_ASSERT((not_<or_<zero, zero, zero, zero, zero> >));
+BOOST_MPL2_ASSERT((not_<or_<zero, zero, zero, zero, zero, zero> >));
+BOOST_MPL2_ASSERT((not_<or_<zero, zero, zero, zero, zero, zero, zero> >));
+BOOST_MPL2_ASSERT((not_<or_<zero, zero, zero, zero, zero, zero, zero, zero> >));
+
+BOOST_MPL2_ASSERT((and_<one, one>));
+BOOST_MPL2_ASSERT((and_<one, one, one>));
+BOOST_MPL2_ASSERT((and_<one, one, one, one>));
 BOOST_MPL2_ASSERT((and_<one, one, one, one, one>));
-BOOST_MPL2_ASSERT((not_<and_<one, one, zero, undefined, one> >));
+BOOST_MPL2_ASSERT((and_<one, one, one, one, one, one>));
+BOOST_MPL2_ASSERT((and_<one, one, one, one, one, one, one>));
+BOOST_MPL2_ASSERT((and_<one, one, one, one, one, one, one, one>));
+
+BOOST_MPL2_ASSERT((not_<and_<one, zero> >));
+BOOST_MPL2_ASSERT((not_<and_<one, one, zero> >));
+BOOST_MPL2_ASSERT((not_<and_<one, one, one, zero> >));
+BOOST_MPL2_ASSERT((not_<and_<one, one, one, one, zero> >));
+BOOST_MPL2_ASSERT((not_<and_<one, one, one, one, one, zero> >));
+BOOST_MPL2_ASSERT((not_<and_<one, one, one, one, one, one, zero> >));
+BOOST_MPL2_ASSERT((not_<and_<one, one, one, one, one, one, one, zero> >));
+
+BOOST_MPL2_ASSERT((or_<one, undefined>));
+BOOST_MPL2_ASSERT((or_<one, undefined, undefined>));
+BOOST_MPL2_ASSERT((or_<one, undefined, undefined, undefined>));
+BOOST_MPL2_ASSERT((or_<one, undefined, undefined, undefined, undefined>));
+BOOST_MPL2_ASSERT((or_<one, undefined, undefined, undefined, undefined, undefined>));
+BOOST_MPL2_ASSERT((or_<one, undefined, undefined, undefined, undefined, undefined, undefined>));
+BOOST_MPL2_ASSERT((or_<one, undefined, undefined, undefined, undefined, undefined, undefined, undefined>));
+
+BOOST_MPL2_ASSERT((not_<and_<zero, undefined> >));
+BOOST_MPL2_ASSERT((not_<and_<zero, undefined, undefined> >));
+BOOST_MPL2_ASSERT((not_<and_<zero, undefined, undefined, undefined> >));
+BOOST_MPL2_ASSERT((not_<and_<zero, undefined, undefined, undefined, undefined> >));
+BOOST_MPL2_ASSERT((not_<and_<zero, undefined, undefined, undefined, undefined, undefined> >));
+BOOST_MPL2_ASSERT((not_<and_<zero, undefined, undefined, undefined, undefined, undefined, undefined> >));
+BOOST_MPL2_ASSERT((not_<and_<zero, undefined, undefined, undefined, undefined, undefined, undefined, undefined> >));
+
+BOOST_MPL2_ASSERT_EXPR((or_<zero, one>{}));
+BOOST_MPL2_ASSERT_EXPR((and_<one, one>{}));
+BOOST_MPL2_ASSERT_EXPR((not_<or_<zero, zero> >{}));
 
 int main()
 {
