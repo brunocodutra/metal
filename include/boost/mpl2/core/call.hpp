689454ac74ac6df119a52b1648f65c673145aa51
Bruno Dutra <brunocodutra@gmail.com>
2015-04-08 20:00:39 -0300

    is_callable<x, args...> now means that call<x, args...>::type is defined

--- include/boost/mpl2/core/call.hpp
+++ include/boost/mpl2/core/call.hpp
@@ -5,14 +5,37 @@
 #ifndef BOOST_MPL2_CORE_CALL_HPP
 #define BOOST_MPL2_CORE_CALL_HPP
 
+#include <boost/mpl2/core/function.hpp>
+#include <boost/mpl2/core/detail/traits.hpp>
+
 namespace boost
 {
     namespace mpl2
     {
+        namespace detail
+        {
+            template<typename func, typename = typename has_template_call<func>::type>
+            struct forward;
+
+            template<typename func>
+            struct forward<func, true_> :
+                    function<func::template call>
+            {};
+
+            template<typename unfunc>
+            struct forward<unfunc, false_>
+            {
+                template<typename...>
+                struct call
+                {};
+            };
+        }
+
         template<typename callable, typename... args>
-        struct call :
-                callable::template call<args...>
-        {};
+        using call = typename detail::forward<callable>::template call<args...>;
+
+        template<typename callable, typename... args>
+        using is_callable = detail::has_type<call<callable, args...> >;
     }
 }
 
--- test/core/call.cpp
+++ test/core/call.cpp
@@ -2,15 +2,31 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
+#include <boost/mpl2/integrals/logical/not.hpp>
 #include <boost/mpl2/core/call.hpp>
 #include <boost/mpl2/core/function.hpp>
+#include <boost/mpl2/core/bind.hpp>
 #include <boost/mpl2/core/assert.hpp>
 
 #include <type_traits>
 
 using namespace boost::mpl2;
 
+BOOST_MPL2_ASSERT((is_callable<function<std::add_pointer>, void>));
+BOOST_MPL2_ASSERT((is_callable<bind<function<std::add_pointer>, void> >));
+
 BOOST_MPL2_ASSERT((std::is_same<call<function<std::add_pointer>, void>::type, void*>));
+BOOST_MPL2_ASSERT((std::is_same<call<bind<function<std::add_pointer>, void> >::type, void*>));
+
+struct uncallable
+{
+    template<typename = void>
+    struct call;
+};
+
+BOOST_MPL2_ASSERT(not_<is_callable<void> >);
+BOOST_MPL2_ASSERT(not_<is_callable<identity<void> > >);
+BOOST_MPL2_ASSERT(not_<is_callable<uncallable> >);
 
 int main()
 {
