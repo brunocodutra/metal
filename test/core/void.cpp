aadbeda8a6108234a27ae0dff21f62106c96f13b
Bruno Dutra <brunocodutra@gmail.com>
2015-04-11 15:13:44 -0300

    generalizing void_ as a function which is not callable for any set of args

--- test/core/void.cpp
+++ test/core/void.cpp
@@ -2,17 +2,12 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
+#include <boost/mpl2/integrals/logical/not.hpp>
 #include <boost/mpl2/core/void.hpp>
 #include <boost/mpl2/core/assert.hpp>
-#include <boost/mpl2/integrals/logical/not.hpp>
 
-using boost::mpl2::void_;
-using boost::mpl2::is_void_;
-using boost::mpl2::not_;
+using namespace boost::mpl2;
 
-BOOST_MPL2_ASSERT(is_void_<void_>);
-BOOST_MPL2_ASSERT(is_void_<void_::type>);
-BOOST_MPL2_ASSERT(not_<is_void_<void> >);
 
 int main()
 {
