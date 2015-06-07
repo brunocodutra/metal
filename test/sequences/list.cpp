cb8767d77b395dcb0ef5eecd7bb72f5fb11bed73
Bruno Dutra <brunocodutra@gmail.com>
2015-06-07 14:44:45 -0300

    reusing test resources

--- test/sequences/list.cpp
+++ test/sequences/list.cpp
@@ -2,8 +2,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
+#include <boost/mpl2/core/comparison/equal_to.hpp>
 #include <boost/mpl2/sequences/list.hpp>
 
+#include "test/integrals.hpp"
 #include "test/main.hpp"
 
 using namespace boost::mpl2;
@@ -12,12 +14,12 @@ using empty = list<>::type;
 using single = list<void>::type;
 using many = list<empty, int, void, single, float>::type;
 
-BOOST_MPL2_ASSERT((std::is_same<many::at<std::integral_constant<std::size_t, 4>>::type, float>));
-BOOST_MPL2_ASSERT((std::is_same<many::at<std::integral_constant<std::size_t, 3>>::type, single>));
-BOOST_MPL2_ASSERT((std::is_same<many::at<std::integral_constant<std::size_t, 2>>::type, void>));
-BOOST_MPL2_ASSERT((std::is_same<many::at<std::integral_constant<std::size_t, 1>>::type, int>));
-BOOST_MPL2_ASSERT((std::is_same<many::at<std::integral_constant<std::size_t, 0>>::type, empty>));
+BOOST_MPL2_ASSERT((std::is_same<many::at<test::zero>::type, empty>));
+BOOST_MPL2_ASSERT((std::is_same<many::at<test::one>::type, int>));
+BOOST_MPL2_ASSERT((std::is_same<many::at<test::two>::type, void>));
+BOOST_MPL2_ASSERT((std::is_same<many::at<test::three>::type, single>));
+BOOST_MPL2_ASSERT((std::is_same<many::at<test::four>::type, float>));
 
-BOOST_MPL2_ASSERT_EXPR(empty::size::value == 0);
-BOOST_MPL2_ASSERT_EXPR(single::size::value == 1);
-BOOST_MPL2_ASSERT_EXPR(many::size::value == 5);
+BOOST_MPL2_ASSERT((equal_to<empty::size, test::zero>));
+BOOST_MPL2_ASSERT((equal_to<single::size, test::one>));
+BOOST_MPL2_ASSERT((equal_to<many::size, test::five>));
