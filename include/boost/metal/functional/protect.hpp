83c57fc1dbb3966466451d8fb7eb3cbdec0a705b
Bruno Dutra <brunocodutra@gmail.com>
2015-06-10 20:31:34 -0300

    making license notice visible to doxygen

--- include/boost/metal/functional/protect.hpp
+++ include/boost/metal/functional/protect.hpp
@@ -1,6 +1,6 @@
-// Copyright Bruno Dutra 2015
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+/// @copyright Bruno Dutra 2015
+/// Distributed under the Boost Software License, Version 1.0.
+/// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
 #ifndef BOOST_METAL_FUNCTIONAL_PROTECT_HPP
 #define BOOST_METAL_FUNCTIONAL_PROTECT_HPP
--- test/src/functional/lambda.cpp
+++ test/src/functional/lambda.cpp
@@ -1,6 +1,6 @@
-// Copyright Bruno Dutra 2015
-// Distributed under the Boost Software License, Version 1.0.
-// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+/// @copyright Bruno Dutra 2015
+/// Distributed under the Boost Software License, Version 1.0.
+/// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
 
 #include <boost/metal/functional/lambda.hpp>
 #include <boost/metal/functional/placeholders.hpp>
