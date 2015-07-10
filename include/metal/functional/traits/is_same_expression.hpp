1ab787f94927f3999ad944fe7dbd328fb8954557
Bruno Dutra <brunocodutra@gmail.com>
2015-07-09 21:02:04 -0300

    introducing trat to compare expressions for equality

---
+++ include/metal/functional/traits/is_same_expression.hpp
@@ -0,0 +1,26 @@
+// Copyright Bruno Dutra 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+#ifndef METAL_FUNCTIONAL_TRAITS_IS_SAME_EXPRESSION_HPP
+#define METAL_FUNCTIONAL_TRAITS_IS_SAME_EXPRESSION_HPP
+
+#include <type_traits>
+
+namespace metal
+{
+    template<template<typename...> class, template<typename...> class>
+    struct is_same_expression :
+            std::false_type
+    {};
+
+    template<template<typename...> class expr>
+    struct is_same_expression<expr, expr> :
+            std::true_type
+    {};
+
+    template<template<typename...> class x, template<typename...> class y>
+    using is_same_expression_t = typename is_same_expression<x, y>::type;
+}
+
+#endif
