f7c587cb26c2c606b50eaf8452bdef99d35e31f1
Bruno Dutra <brunocodutra@gmail.com>
2015-08-18 20:34:58 -0300

    better organizing test targets

---
+++ test/src/metal/map.cpp
@@ -0,0 +1,10 @@
+// Copyright Bruno Dutra 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include <metal/map.hpp>
+
+#include "test.hpp"
+
+using namespace metal;
+using namespace metal::detail;
