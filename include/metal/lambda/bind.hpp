db708d00a3309aa205dcd1cb0ba8139d84b9efab
Bruno Dutra <brunocodutra@gmail.com>
2015-08-29 20:09:20 -0300

    it turns out quasiquote is not very useful if not for binding

--- include/metal/lambda/bind.hpp
+++ include/metal/lambda/bind.hpp
@@ -6,7 +6,8 @@
 #define METAL_LAMBDA_BIND_HPP
 
 #include <metal/lambda/apply.hpp>
-#include <metal/lambda/quasiquote.hpp>
+#include <metal/lambda/lambda.hpp>
+#include <metal/lambda/quote.hpp>
 
 namespace metal
 {
@@ -20,9 +21,41 @@ namespace metal
     template<typename... args>
     using bind_t = typename metal::bind<args...>::type;
 
+    namespace detail
+    {
+        template<typename val>
+        struct quasiquote
+        {
+            using type = val;
+        };
+
+        template<typename... args>
+        using quasiquote_t = typename quasiquote<args...>::type;
+
+        template<
+            template<template<typename...> class> class lambda,
+            template<typename...> class expr
+        >
+        struct quasiquote<lambda<expr>>
+        {
+            template<typename... args>
+            using wrapper = just<expr<args...>>;
+
+            using type = metal::lambda<wrapper>;
+        };
+
+        template<template<typename...> class expr, typename... params>
+        struct quasiquote<expr<params...>>
+        {
+            using type = apply<
+                quote_t<quasiquote_t<lambda<expr>>>,
+                quasiquote_t<params>...
+            >;
+        };
+    }
     template<typename lbd, typename... args>
     struct bind<lbd, args...> :
-            apply<quasiquote_t<lbd>, args...>
+            apply<detail::quasiquote_t<lbd>, args...>
     {};
 }
 
