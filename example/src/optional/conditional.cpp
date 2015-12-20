77ee4bd6557bc51f6f619840974b14a256a67980
Bruno Dutra <brunocodutra@gmail.com>
2015-12-20 17:50:31 -0200

    [doc] adding reference documentation for eval and conditional

---
+++ example/src/optional/conditional.cpp
@@ -0,0 +1,14 @@
+// Copyright Bruno Dutra 2015
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt)
+
+#include <metal/optional/conditional.hpp>
+
+#include "example.hpp"
+
+ANONYMOUS(namespace)
+{
+/// [conditional]
+//TODO
+/// [conditional]
+}
