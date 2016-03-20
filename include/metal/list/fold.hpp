724ab8d93e142a170aa345e9fe1dad357b51b3c5
Bruno Dutra <brunocodutra@gmail.com>
2016-03-20 18:02:45 -0300

    interchange fold<> and accumulate<>
    accumulate<> is now an alias for fold<>

--- include/metal/list/fold.hpp
+++ include/metal/list/fold.hpp
@@ -9,8 +9,14 @@
 
 namespace metal
 {
+    namespace detail
+    {
+        template<typename, typename, typename, typename = nil, typename = nil>
+        struct fold;
+    }
+
     /// \ingroup list
-    /// An alias to metal::accumulate
+    /// ...
     template<
         typename list,
         typename state,
@@ -18,7 +24,7 @@ namespace metal
         typename begin = detail::nil,
         typename end = detail::nil
     >
-    using fold = metal::accumulate<list, state, lbd, begin, end>;
+    using fold = detail::fold<list, state, lbd, begin, end>;
 
     /// \ingroup list
     /// Eager adaptor for \ref fold.
@@ -30,6 +36,99 @@ namespace metal
         typename end = detail::nil
     >
     using fold_t = typename metal::fold<list, state, lbd, begin, end>::type;
+}
+
+#include <metal/list/at.hpp>
+#include <metal/list/size.hpp>
+#include <metal/lambda/arg.hpp>
+#include <metal/lambda/invoke.hpp>
+#include <metal/lambda/lift.hpp>
+#include <metal/number/number.hpp>
+
+namespace metal
+{
+    namespace detail
+    {
+        template<
+            typename list,
+            typename state,
+            typename lbd,
+            typename t, t l, t r
+        >
+        struct fold<list, state, lbd, number<t, l>, number<t, r>> :
+            invoke<
+                fold<
+                    _1,
+                    fold<_1, _2, _3, number<t, l>, number<t, (l + r)/2>>,
+                    _3,
+                    number<t, (l + r)/2>,
+                    number<t, r>
+                >,
+                list, state, lbd
+            >
+        {};
+
+        template<
+            typename list,
+            typename state,
+            typename lbd,
+            typename t, t l
+        >
+        struct fold<list, state, lbd, number<t, l>, number<t, t(l + 1)>> :
+            invoke<lift_t<lbd>, just<state>, at<list, number<t, l>>>
+        {};
+
+        template<
+            typename list,
+            typename state,
+            typename lbd,
+            typename t, t l
+        >
+        struct fold<list, state, lbd, number<t, l>, number<t, t(l - 1)>> :
+            invoke<lift_t<lbd>, just<state>, at<list, number<t, l - 1>>>
+        {};
+
+        template<
+            typename list,
+            typename state,
+            typename lbd,
+            typename t, t l
+        >
+        struct fold<list, state, lbd, number<t, l>, number<t, t(l)>>
+        {
+            using type = state;
+        };
+
+        template<
+            typename list,
+            typename state,
+            typename lbd,
+            typename t, t l,
+            typename u, u r
+        >
+        struct fold<list, state, lbd, number<t, l>, number<u, r>> :
+            fold<
+                list,
+                state,
+                lbd,
+                number<decltype(l + r), l>,
+                number<decltype(l + r), r>
+            >
+        {};
+
+        template<typename list, typename state, typename lbd, typename t, t l>
+        struct fold<list, state, lbd, number<t, l>> :
+            invoke<
+                fold<_1, _2, _3, number<t, l>, size<_1>>,
+                list, state, lbd
+            >
+        {};
+
+        template<typename list, typename state, typename lbd>
+        struct fold<list, state, lbd> :
+            fold<list, state, lbd, integer<0>>
+        {};
+    }
 }
 
 #endif
