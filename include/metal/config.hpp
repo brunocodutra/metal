b1726015827ae4ef50cedfa1763f174ea4efcb68
Bruno Dutra <brunocodutra@gmail.com>
2016-08-22 20:30:10 -0300

    provide version.hpp
    with macros that expand to the current version of the library

---
+++ include/metal/config.hpp
@@ -0,0 +1,13 @@
+// Copyright Bruno Dutra 2015-2016
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
+
+#ifndef METAL_CONFIG_HPP
+#define METAL_CONFIG_HPP
+
+#include <metal/config/version.hpp>
+
+/// \defgroup config Config
+/// \ingroup metal
+
+#endif
