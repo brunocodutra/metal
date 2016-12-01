a7ada2f71beda5caaede14a4cadb41e30a19fc55
Bruno Dutra <brunocodutra@gmail.com>
2016-12-01 22:42:58 +0100

    lift the requirement that join<> must take at least one argument

--- test/src/metal/list/join.cpp
+++ test/src/metal/list/join.cpp
@@ -53,7 +53,7 @@
     CHECK((metal::is_invocable<metal::lambda<metal::join>, LAMBDA(_) COMMA(N) LISTS(N)>), (FALSE)); \
     CHECK((metal::is_invocable<metal::lambda<metal::join>, LAMBDA(_) COMMA(N) MAPS(N)>), (FALSE)); \
     CHECK((metal::is_invocable<metal::lambda<metal::join>, LAMBDA(_) COMMA(N) LAMBDAS(N)>), (FALSE)); \
-    CHECK((metal::join<PAIR(M) COMMA(N) PAIRS(N)>), (metal::list<NUMBER(M), VALUE(M) COMMA(N) ENUM(N, FWD, NUMBER, VALUE)>)); \
+    CHECK((metal::join<PAIRS(M) COMMA(AND(M, N)) PAIRS(N)>), (metal::list<ENUM(M, FWD, NUMBER, VALUE) COMMA(AND(M, N)) ENUM(N, FWD, NUMBER, VALUE)>)); \
     CHECK((metal::join<PAIR(M) COMMA(N) LISTS(N)>), (metal::list<NUMBER(M), VALUE(M) ENUM(N, VALUES)>)); \
     CHECK((metal::join<PAIR(M) COMMA(N) MAPS(N)>), (metal::list<NUMBER(M), VALUE(M) ENUM(N, PAIRS)>)); \
     CHECK((metal::join<ENUM(M, LIST FIX(INC(N))) COMMA(M) ENUM(INC(N), LIST FIX(0))>), (metal::list<ENUM(M, VALUES FIX(INC(N)))>)); \
