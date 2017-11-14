1133752d415d903297652e8560d0bd95b72caddf
Bruno Dutra <brunocodutra@gmail.com>
2017-11-14 23:41:04 +0100

    implicitly enable clang-tidy for all tests

--- Test.cmake
+++ Test.cmake
@@ -6,6 +6,16 @@ include(Configure)
 
 enable_testing()
 
+find_program(CLANG_TIDY NAMES clang-tidy)
+if(CLANG_TIDY)
+    message(STATUS "which clang-tidy: ${CLANG_TIDY}")
+endif()
+
+find_program(CLANG_FORMAT NAMES clang-format)
+if(CLANG_FORMAT)
+    message(STATUS "which clang-format: ${CLANG_FORMAT}")
+endif()
+
 function(test _target)
     set(driver ${_target})
     get_target_property(target_type ${_target} TYPE)
@@ -19,6 +29,12 @@ function(test _target)
 
         if(NOT MSVC)
             target_compile_flags(${_target} PRIVATE -Wall)
+        endif()
+
+        if(CLANG_TIDY)
+            set_target_properties(${_target} PROPERTIES
+                CXX_CLANG_TIDY "${CLANG_TIDY};-checks=-clang-diagnostic-unused-command-line-argument"
+            )
         endif()
 
         add_custom_target(${_target}.build
@@ -211,7 +227,6 @@ function(test_linking _root _lib _prefix)
     endforeach()
 endfunction()
 
-find_program(CLANG_FORMAT NAMES clang-format)
 function(test_formatting _root _lib _prefix)
     set(options)
     set(one_value_args)
