8120f71d514570dbf1615cdf91a81383b6d6216f
Bruno Dutra <brunocodutra@gmail.com>
2018-04-15 16:52:22 +0200

    Squashed 'cmake/' changes from 8f54ac6..32c70f1
    32c70f1 clean up unused features
    cc35fd3 embed license in the bundle
    27bf050 add convenience target bundle
    ed3f9cc remove inline license notices
    dfc8ffd preserve build config when testing
    f3f069c remove unnecessary intermediary test target
    97c50db remove licensing
    327190f rename target_compile_flags to target_weak_compile_options
    5aba129 make target_compile_flags an implementation detail
    1133752 implicitly enable clang-tidy for all tests
    7fb835d implicitly enable warnings for all tests
    5c2740f export targets
    3e7cba0 reduce polution of the top level build directory
    201c561 deploy to the same relative paths on all platforms
    dd35b4c rename singlify to bundle
    3094d20 default versioning compatibility to SameMajorVersion
    40a69fb remove implicitly created deploy target
    555ff5b simplify deploying docs
    b466fe7 provide test_linking to help catching linker errors
    5b22da7 no need to execute header tests
    5232287 fix missing add_dependencies on test helpers
    2114574 prevent naming clashing while testing formatting
    b95f1fa follow cmake best practices when installing targets
    f07dfa2 add target deploy to ALL
    
    git-subtree-dir: cmake
    git-subtree-split: 32c70f17c9a3c2291e493d1d330b56130502de6b

--- Deploy.cmake
+++ Deploy.cmake
@@ -1,33 +1,25 @@
-# Copyright Bruno Dutra 2015-2017
-# Distributed under the Boost Software License, Version 1.0.
-# See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
-
 set(THIS_FILE ${CMAKE_CURRENT_LIST_FILE})
 
-function(singlify _entry _output)
+function(copyright _license _output)
+    set(output "${${_output}}")
+    file(STRINGS ${_license} lines NEWLINE_CONSUME)
+    string(REGEX REPLACE "\n$" "" lines "${lines}")
+    string(REGEX REPLACE "\n" "\n// " output "// ${lines}")
+    set(${_output} "${output}\n\n" PARENT_SCOPE)
+endfunction()
+
+function(bundle _entry _output)
     set(options)
     set(one_value_args EXCLUDED)
-    set(multi_value_args EXCLUDE INCLUDE_DIRS)
+    set(multi_value_args EXCLUDE)
     cmake_parse_arguments(ARGS "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
-
-    if(NOT IS_ABSOLUTE ${_entry})
-        set(_entry "${CMAKE_CURRENT_SOURCE_DIR}/${_entry}")
-    endif()
 
     set(exclude)
     if(ARGS_EXCLUDE)
         set(exclude ${ARGS_EXCLUDE})
     endif()
 
-    get_filename_component(include_dirs ${_entry} DIRECTORY)
-    if(ARGS_INCLUDE_DIRS)
-        foreach(dir ${ARGS_INCLUDE_DIRS})
-            if(NOT IS_ABSOLUTE ${dir})
-                set(dir "${CMAKE_CURRENT_SOURCE_DIR}/${dir}")
-            endif()
-            list(APPEND include_dirs ${dir})
-        endforeach()
-    endif()
+    get_filename_component(include_dir ${_entry} DIRECTORY)
 
     set(output "${${_output}}")
     file(STRINGS ${_entry} lines NEWLINE_CONSUME)
@@ -38,23 +30,16 @@ function(singlify _entry _output)
         set(regex "^[ \t]*#[ \t]*include[ \t]*[\"<]([^\">]*)[\">]")
         if(line MATCHES ${regex})
             string(REGEX REPLACE ${regex} "\\1" include "${line}")
-            foreach(dir ${include_dirs})
-                if(EXISTS "${dir}/${include}")
-                    set(next "${dir}/${include}")
-                    get_filename_component(next "${next}" ABSOLUTE)
-                    break()
-                endif()
-            endforeach()
+            if(NOT IS_ABSOLUTE "${include}" AND EXISTS "${include_dir}/${include}")
+                set(next "${include_dir}/${include}")
+                get_filename_component(next "${next}" ABSOLUTE)
+            endif()
         endif()
 
         if(next)
             if(NOT next IN_LIST exclude)
                 list(APPEND exclude ${next})
-                singlify(${next} output
-                    EXCLUDE ${exclude}
-                    EXCLUDED exclude
-                    INCLUDE_DIRS ${ARGS_INCLUDE_DIRS}
-                )
+                bundle(${next} output EXCLUDE ${exclude} EXCLUDED exclude)
             endif()
         else()
             set(output "${output}${line}\n")
@@ -68,140 +53,93 @@ function(singlify _entry _output)
     set(${_output} "${output}" PARENT_SCOPE)
 endfunction()
 
-function(add_header_library _name _entry)
+function(add_header_library _name _entry _license)
+    if(NOT IS_ABSOLUTE ${_entry})
+        set(_entry "${CMAKE_CURRENT_SOURCE_DIR}/${_entry}")
+    endif()
+
+    if(NOT IS_ABSOLUTE ${_license})
+        set(_license "${CMAKE_CURRENT_SOURCE_DIR}/${_license}")
+    endif()
+
     get_filename_component(output ${_entry} NAME)
-    set(output_dir "${CMAKE_CURRENT_BINARY_DIR}/singlified/${_name}")
+    set(output_dir "${CMAKE_BINARY_DIR}/${_name}/include")
     set(output "${output_dir}/${output}")
 
     get_filename_component(include_dir ${_entry} DIRECTORY)
     file(GLOB_RECURSE headers "${include_dir}/*.hpp")
 
-    set(singlify_cmake "${CMAKE_CURRENT_BINARY_DIR}/${_name}.singlify.cmake")
-    file(WRITE ${singlify_cmake} "\
+    set(bundle "${CMAKE_BINARY_DIR}/${_name}/bundle.cmake")
+    file(WRITE ${bundle} "\
 cmake_minimum_required(VERSION ${CMAKE_VERSION})
 include(${THIS_FILE})
-singlify(${_entry} single INCLUDE_DIRS ${include_dir})
-file(WRITE \"${output}\" \"\${single}\")
+copyright(${_license} output)
+bundle(${_entry} output)
+file(WRITE \"${output}\" \"\${output}\")
 "
     )
 
-    add_custom_command(OUTPUT ${output}
-        COMMAND ${CMAKE_COMMAND} -P ${singlify_cmake}
-        COMMENT "singlifying ${_name}..."
+    add_custom_command(
+        OUTPUT ${output}
+        COMMAND ${CMAKE_COMMAND} -P ${bundle}
+        COMMENT "bundling ${_name}..."
         DEPENDS ${headers}
     )
 
-    add_custom_target(${_name}.singlify DEPENDS ${output} SOURCES ${headers})
+    add_custom_target(${_name}.bundle DEPENDS ${output} SOURCES ${headers})
 
     add_library(${_name} INTERFACE)
-    set_target_properties(${_name} PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES ${output_dir}
-    )
+    target_include_directories(${_name} INTERFACE $<BUILD_INTERFACE:${output_dir}>)
 
-    add_dependencies(${_name} ${_name}.singlify)
+    if (NOT TARGET bundle)
+        add_custom_target(bundle ALL)
+    endif()
+
+    add_dependencies(bundle ${_name}.bundle)
+    add_dependencies(${_name} ${_name}.bundle)
 endfunction()
+
+include(CMakePackageConfigHelpers)
 
 function(deploy_header_library _lib)
     get_target_property(name ${_lib} NAME)
     get_target_property(version ${_lib} INTERFACE_LIB_VERSION)
-    get_target_property(features ${_lib} INTERFACE_COMPILE_FEATURES)
-    get_target_property(include_dirs ${_lib} INTERFACE_INCLUDE_DIRECTORIES)
+    get_target_property(dirs ${_lib} INTERFACE_INCLUDE_DIRECTORIES)
 
-    if(WIN32 AND NOT CYGWIN)
-        set(cmake_install_dir CMake)
-        set(include_install_dir "${name}/include")
-    else()
-        set(cmake_install_dir "share/cmake/${name}")
-        set(include_install_dir include)
-    endif()
+    set(include_dest "include")
+    set(cmake_dest "lib/cmake/${name}")
+    set(cmake_src "${CMAKE_BINARY_DIR}/${name}/${cmake_dest}")
 
-    file(RELATIVE_PATH relative_include_install_dir
-        /${cmake_install_dir} /${include_install_dir}
+    set(config_file "${cmake_src}/${name}Config.cmake")
+    set(version_file "${cmake_src}/${name}ConfigVersion.cmake")
+    set(targets_file "${cmake_src}/${name}Targets.cmake")
+
+    file(WRITE ${config_file} "include(\${CMAKE_CURRENT_LIST_DIR}/${name}Targets.cmake)")
+
+    write_basic_package_version_file(${version_file}
+        VERSION ${version}
+        COMPATIBILITY SameMajorVersion
     )
 
-    set(config_file "${CMAKE_CURRENT_BINARY_DIR}/${name}Config.cmake")
-    set(version_file "${CMAKE_CURRENT_BINARY_DIR}/${name}ConfigVersion.cmake")
+    install(FILES ${config_file} ${version_file} DESTINATION ${cmake_dest})
+    install(TARGETS ${_lib} EXPORT ${name}Targets INCLUDES DESTINATION ${include_dest})
+    install(EXPORT ${name}Targets DESTINATION ${cmake_dest})
+    export(TARGETS ${_lib} FILE ${targets_file})
 
-    file(WRITE ${config_file} "\
-# ${name} configuration file
-#
-# Defines the CMake target ${name}
-
-add_library(${name} INTERFACE IMPORTED)
-set_target_properties(${name} PROPERTIES
-    INTERFACE_LIB_VERSION ${version}
-    INTERFACE_COMPILE_FEATURES ${features}
-    INTERFACE_INCLUDE_DIRECTORIES \"\${CMAKE_CURRENT_LIST_DIR}/${relative_include_install_dir}/\"
-)
-"
-    )
-
-    file(WRITE ${version_file} "\
-# ${name} version file
-
-set(PACKAGE_VERSION ${version})
-
-if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
-    set(PACKAGE_VERSION_COMPATIBLE FALSE)
-else()
-    set(PACKAGE_VERSION_COMPATIBLE TRUE)
-    if(PACKAGE_VERSION VERSION_EQUAL PACKAGE_FIND_VERSION)
-        set(PACKAGE_VERSION_EXACT TRUE)
-    endif()
-endif()
-"
-    )
-
-    set(dist "${PROJECT_BINARY_DIR}/dist/")
-    set(dist_cmake "${dist}/${cmake_install_dir}/")
-    set(dist_include "${dist}/${include_install_dir}/")
-
-    add_custom_target(${name}.deploy
-        COMMAND ${CMAKE_COMMAND} -E remove_directory ${dist_cmake}
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${dist_cmake}
-        COMMAND ${CMAKE_COMMAND} -E copy ${config_file} ${dist_cmake}
-        COMMAND ${CMAKE_COMMAND} -E copy ${version_file} ${dist_cmake}
-        COMMAND ${CMAKE_COMMAND} -E remove_directory ${dist_include}
-        COMMAND ${CMAKE_COMMAND} -E make_directory ${dist_include}
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${include_dirs} ${dist_include}
-        COMMENT "deploying ${name}..."
-        DEPENDS ${_lib}
-    )
-
-    install(DIRECTORY ${dist} DESTINATION .)
-
-    if(NOT TARGET deploy)
-        add_custom_target(deploy)
-    endif()
-
-    add_dependencies(deploy ${name}.deploy)
+    foreach(dir ${dirs})
+        install(DIRECTORY ${dir}/ DESTINATION ${include_dest})
+    endforeach()
 endfunction()
 
 function(deploy_doc _lib _doc)
     get_target_property(name ${_lib} NAME)
-    get_target_property(doc ${_doc} OUTPUT)
+    get_target_property(dir ${_doc} OUTPUT)
 
     if(WIN32 AND NOT CYGWIN)
-        set(doc_install_dir "${name}/doc")
+        set(install_dir "${name}/doc")
     else()
-        set(doc_install_dir "share/${name}/doc")
+        set(install_dir "share/${name}/doc")
     endif()
 
-    set(dist "${PROJECT_BINARY_DIR}/dist/")
-    set(dist_doc "${dist}/${doc_install_dir}/")
-
-    add_custom_target(${_doc}.deploy
-        COMMAND ${CMAKE_COMMAND} -E remove_directory ${dist_doc}
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${doc} ${dist_doc}
-        COMMENT "deploying ${name} docs..."
-        DEPENDS ${_doc}
-    )
-
-    install(DIRECTORY ${dist} DESTINATION .)
-
-    if(NOT TARGET deploy)
-        add_custom_target(deploy)
-    endif()
-
-    add_dependencies(deploy ${_doc}.deploy)
+    install(DIRECTORY ${dir}/ DESTINATION ${install_dir} OPTIONAL)
 endfunction()
--- Test.cmake
+++ Test.cmake
@@ -1,25 +1,62 @@
-# Copyright Bruno Dutra 2015-2017
-# Distributed under the Boost Software License, Version 1.0.
-# See accompanying file LICENSE.txt or copy at http://boost.org/LICENSE_1_0.txt
-
 enable_testing()
 
-function(test _target)
-    get_target_property(target_type ${_target} TYPE)
-    if(target_type STREQUAL "EXECUTABLE")
-        add_custom_target(${_target}.driver
-            COMMAND ${CMAKE_COMMAND} --build . --target ${_target} --config ${CMAKE_BUILD_TYPE}
-            COMMAND "$<TARGET_FILE:${_target}>"
-            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-        )
-    else()
-        add_custom_target(${_target}.driver
-            COMMAND ${CMAKE_COMMAND} --build . --target ${_target} --config ${CMAKE_BUILD_TYPE}
-            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-        )
+find_program(CLANG_TIDY NAMES clang-tidy)
+if(CLANG_TIDY)
+    message(STATUS "which clang-tidy: ${CLANG_TIDY}")
+endif()
+
+find_program(CLANG_FORMAT NAMES clang-format)
+if(CLANG_FORMAT)
+    message(STATUS "which clang-format: ${CLANG_FORMAT}")
+endif()
+
+include(CheckCXXCompilerFlag)
+function(target_weak_compile_options _target _visibility _flag)
+    set(result "has${_flag}")
+    string(REGEX REPLACE "[ ]" "" result "${result}")
+    string(REGEX REPLACE "[+]" "x" result "${result}")
+    string(REGEX REPLACE "[^a-zA-Z0-9_]" "_" result "${result}")
+
+    check_cxx_compiler_flag(${_flag} ${result})
+    if(${result})
+        target_compile_options(${_target} ${_visibility} ${_flag})
     endif()
+endfunction()
+
+function(test _target)
+
+    get_target_property(target_type ${_target} TYPE)
+
+    if(target_type STREQUAL "EXECUTABLE"
+    OR target_type STREQUAL "STATIC_LIBRARY"
+    OR target_type STREQUAL "SHARED_LIBRARY")
+        target_weak_compile_options(${_target} PRIVATE -W)
+        target_weak_compile_options(${_target} PRIVATE -Wextra)
+        target_weak_compile_options(${_target} PRIVATE -Wpedantic)
+
+        if(NOT MSVC)
+            target_weak_compile_options(${_target} PRIVATE -Wall)
+        endif()
+
+        if(CLANG_TIDY)
+            set_target_properties(${_target} PROPERTIES
+                CXX_CLANG_TIDY "${CLANG_TIDY};-checks=-clang-diagnostic-unused-command-line-argument"
+            )
+        endif()
+    endif()
+
+    set(driver ${_target})
+    if(target_type STREQUAL "EXECUTABLE")
+        add_custom_target(${_target}.run
+            COMMAND ${_target}
+            WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+        )
+        add_dependencies(${_target}.run ${_target})
+        set(driver ${_target}.run)
+    endif()
+
     add_test(NAME ${_target}
-        COMMAND ${CMAKE_COMMAND} --build . --target ${_target}.driver
+        COMMAND ${CMAKE_COMMAND} --build . --config $<CONFIG> --target ${driver}
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
     )
 endfunction()
@@ -70,7 +107,7 @@ function(test_units _root _lib _prefix)
         set(node "${_prefix}/${node}")
         if(EXISTS "${node}.cpp")
             add_executable(${target} EXCLUDE_FROM_ALL "${node}.cpp")
-            target_link_libraries(${target} ${_lib})
+            target_link_libraries(${target} PRIVATE ${_lib})
         endif()
 
         if(IS_DIRECTORY ${node})
@@ -78,6 +115,7 @@ function(test_units _root _lib _prefix)
         endif()
 
         if(TARGET ${target})
+            add_dependencies(${target} ${_lib})
             add_dependencies(${_root} ${target})
             test(${target})
         endif()
@@ -118,9 +156,9 @@ function(test_headers _root _lib _prefix)
         set(node "${_prefix}/${node}")
         if(EXISTS "${node}.hpp")
             set(src "${CMAKE_CURRENT_BINARY_DIR}/${target}.cpp")
-            file(WRITE ${src} "#include \"${node}.hpp\"\nint main(){return 0;}\n")
-            add_executable(${target} EXCLUDE_FROM_ALL ${src})
-            target_link_libraries(${target} ${_lib})
+            file(WRITE ${src} "#include \"${node}.hpp\"")
+            add_library(${target} STATIC EXCLUDE_FROM_ALL ${src})
+            target_link_libraries(${target} PRIVATE ${_lib})
         endif()
 
         if(IS_DIRECTORY ${node})
@@ -128,13 +166,68 @@ function(test_headers _root _lib _prefix)
         endif()
 
         if(TARGET ${target})
+            add_dependencies(${target} ${_lib})
             add_dependencies(${_root} ${target})
             test(${target})
         endif()
     endforeach()
 endfunction()
 
-find_program(CLANG_FORMAT NAMES clang-format)
+function(test_linking _root _lib _prefix)
+    set(options)
+    set(one_value_args)
+    set(multi_value_args EXCLUDE)
+    cmake_parse_arguments(ARGS "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
+
+    if(NOT ARGS_EXCLUDE)
+        set(ARGS_EXCLUDE "^$")
+    else()
+        string(REPLACE ";" "|" ARGS_EXCLUDE "${ARGS_EXCLUDE}")
+    endif()
+
+    if(NOT IS_ABSOLUTE ${_prefix})
+        set(_prefix "${CMAKE_CURRENT_SOURCE_DIR}/${_prefix}")
+    endif()
+
+    if(NOT IS_DIRECTORY ${_prefix} OR NOT EXISTS ${_prefix})
+        message(FATAL_ERROR "'${_prefix}' is not a valid directory.")
+    endif()
+
+    if(NOT TARGET ${_root})
+        add_custom_target(${_root})
+    endif()
+
+    get_tree_nodes(${_prefix} nodes)
+    foreach(node ${nodes})
+        if(node MATCHES ${ARGS_EXCLUDE})
+            continue()
+        endif()
+
+        set(target ${_root}.${node})
+        set(node "${_prefix}/${node}")
+        if(EXISTS "${node}.hpp")
+            set(src_a "${CMAKE_CURRENT_BINARY_DIR}/${target}.a.cpp")
+            set(src_b "${CMAKE_CURRENT_BINARY_DIR}/${target}.b.cpp")
+
+            file(WRITE ${src_a} "#include \"${node}.hpp\"")
+            file(WRITE ${src_b} "#include \"${node}.hpp\"")
+
+            add_library(${target} SHARED EXCLUDE_FROM_ALL ${src_a} ${src_b})
+            target_link_libraries(${target} PRIVATE ${_lib})
+        endif()
+
+        if(IS_DIRECTORY ${node})
+            test_linking(${target} ${_lib} ${node} ${ARGN})
+        endif()
+
+        if(TARGET ${target})
+            add_dependencies(${target} ${_lib})
+            add_dependencies(${_root} ${target})
+            test(${target})
+        endif()
+    endforeach()
+endfunction()
+
 function(test_formatting _root _lib _prefix)
     set(options)
     set(one_value_args)
@@ -166,7 +259,7 @@ function(test_formatting _root _lib _prefix)
         endif()
 
         set(target ${_root}.${node})
-        set(formatted "${CMAKE_CURRENT_BINARY_DIR}/${node}.formatted.hpp")
+        set(formatted "${CMAKE_CURRENT_BINARY_DIR}/${target}.hpp")
         set(node "${_prefix}/${node}")
         set(original "${node}.hpp")
         if(EXISTS ${original})
@@ -203,7 +296,7 @@ cmake_minimum_required(VERSION ${CMAKE_VERSION})
 project(dependent CXX)
 find_package(${_lib} ${_version} EXACT REQUIRED)
 add_executable(dependent main.cpp)
-target_link_libraries(dependent ${_lib})
+target_link_libraries(dependent PRIVATE ${_lib})
 "
     )
 
@@ -213,7 +306,7 @@ target_link_libraries(dependent ${_lib})
 
     ExternalProject_Add(${_target}
         URL ""
-        DEPENDS deploy
+        DEPENDS ${_lib}
         BUILD_ALWAYS 1
         EXCLUDE_FROM_ALL 1
         PREFIX ${prefix}
