56fccbe88273a5d2f4545f0988abc81c50ff04c2
Breno Guimarães <brenorg@gmail.com>
2019-02-02 01:49:20 +0000

    Support sorting large lists by dividing and conquering on merge step of mergesort
    This change generalizes the merge step of mergesort to what is being called "partial merge".
    
    Partial merge algorithm receives two lists and a comparison function. Contrary to regular merge, it assumes there are extra elements after the input lists:
    
    merge([e1, e2, e3, e4], [e5, e6, e7 e8], cmp) -> partial_merge([e1, e2, ?], [e5, e6, ?]) + partial_merge(...)
    
    The algorithm can't compare an element to ?, therefore it returns two things:
    1 - The partial result it was able to merge
    2 - The "carry" which was whatever leftover of the inputs when it comes to compare elements with ?
    
    In this algorithm, the two input lists are divided in two, and partial merge is applied to the first half of both lists.
    The carry is appended to the second half of the input lists, and then partial merge is applied again.
    The partial result of the invocation is the concatenation of the partial results of both partial merge recursive invocations.
    
    The result of the partial merge invocation is the concatenation of the result of the two recursive invocations.
    The carry is the carry of the second recursive invocation.
    
    In this implementation, when the input lists are smaller than a given threshold, the algorithm falls back to the simpler and direct version of partial merge.
    By doing so, there is a minimal cost of applying the divide/conquer for small lists. For large lists, the recursion depth will be limited, allowing the compiler to sort lists of thousands of elements.

--- include/metal/list/sort.hpp
+++ include/metal/list/sort.hpp
@@ -49,6 +49,7 @@ namespace metal {
 }
 
 #include "../lambda/lambda.hpp"
+#include "../list/join.hpp"
 #include "../list/list.hpp"
 #include "../list/range.hpp"
 #include "../number/if.hpp"
@@ -57,6 +58,7 @@ namespace metal {
 namespace metal {
     /// \cond
     namespace detail {
+
         template<typename>
         struct cons {};
 
@@ -75,35 +77,116 @@ namespace metal {
         template<
             typename, typename, typename, template<typename...> class,
             typename = true_>
-        struct _merge {};
+        struct _partial_merge {};
 
         template<
             typename x, typename y, typename... zs,
             template<typename...> class e>
-        struct _merge<
+        struct _partial_merge<
             x, y, list<zs...>, e, if_<call<e, head<y>, head<x>>, true_, false_>>
-            : _merge<x, tail<y>, list<zs..., head<y>>, e> {};
+            : _partial_merge<x, tail<y>, list<zs..., head<y>>, e> {};
 
         template<
             typename x, typename y, typename... zs,
             template<typename...> class e>
-        struct _merge<
+        struct _partial_merge<
             x, y, list<zs...>, e, if_<call<e, head<y>, head<x>>, false_, true_>>
-            : _merge<tail<x>, y, list<zs..., head<x>>, e> {};
+            : _partial_merge<tail<x>, y, list<zs..., head<x>>, e> {};
 
         template<typename... xs, typename... zs, template<typename...> class e>
-        struct _merge<list<xs...>, list<>, list<zs...>, e> {
-            using type = list<zs..., xs...>;
+        struct _partial_merge<list<xs...>, list<>, list<zs...>, e> {
+            using type = list<zs...>;
+            using l1_carry = list<xs...>;
+            using l2_carry = list<>;
         };
 
         template<typename... ys, typename... zs, template<typename...> class e>
-        struct _merge<list<>, list<ys...>, list<zs...>, e> {
-            using type = list<zs..., ys...>;
+        struct _partial_merge<list<>, list<ys...>, list<zs...>, e> {
+            using type = list<zs...>;
+            using l1_carry = list<>;
+            using l2_carry = list<ys...>;
         };
 
         template<typename z, template<typename...> class e>
-        struct _merge<list<>, list<>, z, e> {
+        struct _partial_merge<list<>, list<>, z, e> {
             using type = z;
+            using l1_carry = list<>;
+            using l2_carry = list<>;
+        };
+
+        template<class, class, typename = true_>
+        struct _large_partial_merge {};
+
+        template<class... e1s, class... e2s>
+        struct _large_partial_merge<
+            list<e1s...>, list<e2s...>,
+            number<(
+                (sizeof...(e1s) >= 2) && (sizeof...(e2s) >= 2)
+                && (sizeof...(e1s) + sizeof...(e2s)) > 512)>> {
+            using l1 = list<e1s...>;
+            using l2 = list<e2s...>;
+
+            using l1_half1 = range<l1, number<0>, number<sizeof...(e1s) / 2>>;
+            using l2_half1 = range<l2, number<0>, number<sizeof...(e2s) / 2>>;
+
+            using l1_half2 =
+                range<l1, number<sizeof...(e1s) / 2>, number<sizeof...(e1s)>>;
+            using l2_half2 =
+                range<l2, number<sizeof...(e2s) / 2>, number<sizeof...(e2s)>>;
+
+            template<template<class...> class cmp>
+            using sub_merge1 = _large_partial_merge<l1_half1, l2_half1>;
+
+            template<template<class...> class cmp>
+            using l1_half2_with_carry = join<
+                typename sub_merge1<cmp>::template l1_carry<cmp>, l1_half2>;
+
+            template<template<class...> class cmp>
+            using l2_half2_with_carry = join<
+                typename sub_merge1<cmp>::template l2_carry<cmp>, l2_half2>;
+
+            template<template<class...> class cmp>
+            using sub_merge2 = _large_partial_merge<
+                l1_half2_with_carry<cmp>, l2_half2_with_carry<cmp>>;
+
+            template<template<class...> class cmp>
+            using type = join<
+                typename sub_merge1<cmp>::template type<cmp>,
+                typename sub_merge2<cmp>::template type<cmp>>;
+
+            template<template<class...> class cmp>
+            using l1_carry = typename sub_merge2<cmp>::template l1_carry<cmp>;
+
+            template<template<class...> class cmp>
+            using l2_carry = typename sub_merge2<cmp>::template l2_carry<cmp>;
+        };
+
+        template<class... e1s, class... e2s>
+        struct _large_partial_merge<
+            list<e1s...>, list<e2s...>,
+            number<!(
+                (sizeof...(e1s) >= 2) && (sizeof...(e2s) >= 2)
+                && (sizeof...(e1s) + sizeof...(e2s)) > 512)>> {
+            template<template<class...> class cmp>
+            using type = typename _partial_merge<
+                list<e1s...>, list<e2s...>, list<>, cmp>::type;
+
+            template<template<class...> class cmp>
+            using l1_carry = typename _partial_merge<
+                list<e1s...>, list<e2s...>, list<>, cmp>::l1_carry;
+
+            template<template<class...> class cmp>
+            using l2_carry = typename _partial_merge<
+                list<e1s...>, list<e2s...>, list<>, cmp>::l2_carry;
+        };
+
+        template<class l1, class l2, template<class...> class e>
+        struct _merge {
+            using sub_merge = _large_partial_merge<l1, l2>;
+            using type = join<
+                forward<sub_merge::template type, e>,
+                forward<sub_merge::template l1_carry, e>,
+                forward<sub_merge::template l2_carry, e>>;
         };
 
         template<typename seq>
@@ -120,7 +203,7 @@ namespace metal {
             using type = typename _merge<
                 forward<_sort_impl<range<seq, beg, mid>>::template type, expr>,
                 forward<_sort_impl<range<seq, mid, end>>::template type, expr>,
-                list<>, expr>::type;
+                expr>::type;
         };
 
         template<typename x, typename y>
