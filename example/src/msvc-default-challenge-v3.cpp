e260b47b35b64a4cfc8958738f0da82ad2ea461c
ecrypa <>
2019-10-16 21:02:57 +0200

    try multiple defaulted template alias parameters for MSVC 14

---
+++ example/src/msvc-default-challenge-v3.cpp
@@ -0,0 +1,40 @@
+#include "example.hpp"
+
+#include <cstdint>
+#include <memory>
+#include <metal.hpp>
+#include <tuple>
+
+namespace metal {
+    namespace v3 {
+        namespace detail {
+        template<class seq>
+        struct _sort {};
+
+        template<class... vals>
+        struct _sort<list<vals...>> {
+          template<class>
+          using type = void;
+        };
+        } // detail
+
+    using default_comp = metal::lambda<metal::less>;
+
+    template<class seq, class lbd = default_comp>
+    using sort = metal::detail::call<detail::_sort<seq>::template type, lbd>;
+    } // v?
+} // metal
+
+HIDE(
+/// [iota]
+IS_SAME(
+  metal::v3::sort<metal::numbers<7, -4, 2>, metal::lambda<metal::less>>,
+  void
+);
+
+IS_SAME(
+  metal::v3::sort<metal::numbers<7, -4, 2>>,
+  void
+);
+/// [iota]
+)
