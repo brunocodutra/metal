1251903c1fb17858b35d0723adef6168e413c19b
ecrypa <>
2019-10-27 18:26:12 +0100

    unconditionally define METAL_WORKAROUND

--- include/metal/config/config.hpp
+++ include/metal/config/config.hpp
@@ -1,11 +1,6 @@
 #ifndef METAL_CONFIG_CONFIG_HPP
 #define METAL_CONFIG_CONFIG_HPP
 
-#if !defined(METAL_WORKAROUND)
-#if(defined(__GNUC__) && !defined(__clang__) && (__GNUC__ < 5)) \
-    || (defined(_MSC_VER) && !defined(__clang__))
 #define METAL_WORKAROUND
-#endif
-#endif
 
 #endif
