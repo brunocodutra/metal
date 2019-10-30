92bc5ea6fb81ae6adf2ae0886cc88efd78f4ed99
ecrypa <>
2019-10-30 23:57:43 +0100

    fix custom type switch of metal::partition for MSVC

--- include/metal/list/partition.hpp
+++ include/metal/list/partition.hpp
@@ -62,14 +62,17 @@ namespace metal {
             using type = pair<join<left...>, join<right...>>;
         };
 
-        template<bool>
-        struct _partition_filter {
+        template<class>
+        struct _partition_filter {};
+
+        template<>
+        struct _partition_filter<true_> {
             template<class val>
             using type = list<list<val>, list<>>;
         };
 
         template<>
-        struct _partition_filter<false> {
+        struct _partition_filter<false_> {
             template<class val>
             using type = list<list<>, list<val>>;
         };
@@ -79,8 +82,8 @@ namespace metal {
 
         template<int_... ns, class... vals>
         struct _partitioner<list<number<ns>...>, list<vals...>>
-            : _partition_joiner<
-                  call<_partition_filter<!!ns>::template type, vals>...> {};
+            : _partition_joiner<call<
+                  _partition_filter<number<!!ns>>::template type, vals>...> {};
 
         template<class seq>
         struct _partition_impl {};
